6c7d6da849d8ff45189765caec6162de4a807408
htunhtunhtet <ryanhhh91@gmail.com>
2022-10-24 00:47:20 +0800

    Make jquery global use

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 module.exports = {
     entry: './src/index.js',
     mode: 'development', // 'production' <- production flag
@@ -6,4 +7,10 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery', //jquery
+        })
+    ]
 };
